b7eea7a1a1ce41f45ff0c685d4f8ccc785e99150
lamiasalih2024-ctrl <lamiasalih2024@gmail.com>
2026-06-17 22:25:20 +0300

    Update API to filter World Cup 2026

--- api/football.js
+++ api/football.js
@@ -1,18 +1,13 @@
 export default async function handler(req, res) {
-  // تأكدي أن الاسم هنا هو نفس الاسم الذي كتبتيه في Vercel (MY_API_KEY)
-  const apiKey = process.env.MY_API_KEY; 
-  const endpoint = 'https://v3.football.api-sports.io/fixtures?live=all';
-
-  try {
-    const response = await fetch(endpoint, {
-      headers: { 
-        'x-rapidapi-key': apiKey,
-        'x-rapidapi-host': 'v3.football.api-sports.io'
-      }
-    });
-    const data = await response.json();
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ error: 'فشل الاتصال' });
-  }
+  const url = 'https://v3.football.api-sports.io/fixtures?live=all&league=1&season=2026';
+  
+  const response = await fetch(url, {
+    headers: {
+      'x-rapidapi-key': 'هنا_ضعي_مفتاحك_السري',
+      'x-rapidapi-host': 'v3.football.api-sports.io'
+    }
+  });
+  
+  const data = await response.json();
+  res.status(200).json(data);
 }
