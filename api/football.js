73f02df7dc9b81054dd6dcfc1ddff980222e32d8
lamiasalih2024-ctrl <lamiasalih2024@gmail.com>
2026-06-17 15:58:38 +0300

    Update football.js

--- api/football.js
+++ api/football.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   const apiKey = process.env.API_KEY; 
-  // هذا الرابط المباشر لكأس العالم 2026
-  const endpoint = 'https://v3.football.api-sports.io/leagues?name=World%20Cup&season=2026';
+  // قمت بتغيير الرابط ليجلب بيانات كأس العالم 2026 برقم البطولة الموحد (1)
+  const endpoint = 'https://v3.football.api-sports.io/leagues?id=1';
 
   try {
     const response = await fetch(endpoint, {
