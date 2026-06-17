746f5bf55c36b3e81d61b2058b5db6bed88085d0
lamiasalih2024-ctrl <lamiasalih2024@gmail.com>
2026-06-17 23:37:28 +0300

    Update football.js

--- api/football.js
+++ api/football.js
@@ -1,13 +1,14 @@
 export default async function handler(req, res) {
-  const url = 'https://v3.football.api-sports.io/fixtures?live=all&league=1&season=2026';
+  const url = 'https://v3.football.api-sports.io/fixtures?live=all&league=1';
   
   const response = await fetch(url, {
+    method: 'GET',
     headers: {
-      'x-rapidapi-key': 'هنا_ضعي_مفتاحك_السري',
-      'x-rapidapi-host': 'v3.football.api-sports.io'
+      'x-rapidapi-host': 'v3.football.api-sports.io',
+      'x-rapidapi-key': process.env.API_KEY // تأكدي أن هذا المتغير مضبوط عندك في Vercel
     }
   });
-  
+
   const data = await response.json();
   res.status(200).json(data);
 }
